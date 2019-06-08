0c0fb627470e27088964a7122880ea8ea2f43086
TuttleStripes <46587239+TuttleStripes@users.noreply.github.com>
2019-06-08 14:05:10 -0500

    Add files via upload

---
+++ location2.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import json
+import re
+import requests
+import time
+
+
+with open(r'C:\Users\Peter\Documents\Python\Fallout 4\cells.json') as f:
+    cells = json.load(f)
+cache = set()
+rxloc = re.compile(r'\[Cell <(\w+?) \([0-9A-F]{8}\)>\]')
+
+
+def grabInfo(url):
+    try:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        loot = soup.find(text='Notable loot')
+        if loot is not None:
+            loot = loot.findAllNext()[3].getText().strip().split('\n')
+            for i, v in enumerate(loot):
+                loot[i] = ':: ' + v
+            return '\n'.join(loot)
+        else:
+            return None
+    except ConnectionError:
+        return None
+
+
+if __name__ == "__main__":
+    while True:
+        with open(r'C:\Users\Peter\Documents\My Games\Fallout4\Logs\Script\Papyrus.0.log') as f:
+            location = rxloc.findall(f.read())[-1]
+            if location in cells and cells[location] not in cache:
+                print(f'\n\n\033[96m{location}\033[0m')
+                loot = grabInfo(f'https://fallout.fandom.com{cells[location]}')
+                if loot is not None:
+                    print(loot)
+                else:
+                    print('There is no notable loot here')
+                cache.add(cells[location])
+        time.sleep(10)
