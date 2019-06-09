6cfd67fbee104abb4e79b8a3f6ac77d2faf632c0
TuttleStripes <46587239+TuttleStripes@users.noreply.github.com>
2019-06-08 20:33:12 -0500

    Add files via upload

--- location2.py
+++ location2.py
@@ -6,12 +6,15 @@ import requests
 import time
 
 
-with open('cells.json') as f:
+with open(r'C:\Users\Peter\Documents\Python\Fallout 4\cells.json') as f:
     cells = json.load(f)
 rxloc = re.compile(r'\[Cell <(\w+?) \([0-9A-F]{8}\)>\]')
 
 
 def grabInfo(url):
+    """grabs the "Notable loot" section from https://fallout.fandom.com if it exists
+    Returns the loot if it exists, or None if it doesn't
+    """
     try:
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
         loot = soup.find(text='Notable loot')
@@ -26,18 +29,35 @@ def grabInfo(url):
         return None
 
 
-if __name__ == "__main__":
-    cache = [None]
+def followLog(logfile):
+    """tails the file, blatently stolen from https://github.com/dabeaz/generators/blob/master/examples/follow.py
+    used so I don't have to read the entire file each time
+    yields end line
+    """
+    logfile.seek(0, os.SEEK_END)
     while True:
-        home = os.path.expanduser('~')
-        with open(os.path.join(home, 'Documents\\My Games\\Fallout4\\Logs\\Script\\Papyrus.0.log')) as f:
-            location = rxloc.findall(f.read())[-1]
-            if location in cells and cells[location] != cache[-1]:
+        line = logfile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+
+if __name__ == "__main__":
+    mostrecent = None
+    home = os.path.expanduser('~')
+    with open(os.path.join(home, 'Documents\\My Games\\Fallout4\\Logs\\Script\\Papyrus.0.log')) as f:
+        for line in followLog(f):
+            try:
+                location = re.search(r'\[Cell <(.+?) \(', line)[1]
+            except TypeError:
+                continue
+            if location in cells and cells[location] != mostrecent:
                 print(f'\n\n\033[96m{location}\033[0m')
                 loot = grabInfo(f'https://fallout.fandom.com{cells[location]}')
                 if loot is not None:
                     print(loot)
                 else:
                     print('There is no notable loot here')
-                cache.append(cells[location])
-        time.sleep(3)
+                mostrecent = cells[location]
